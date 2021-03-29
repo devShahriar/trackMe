55969ecdf5de048c9501ca646e74c8ea20babdda
Shahriar-shudip <shudip.shahriar@gmail.com>
2021-03-30 05:03:13 +0600

    -

--- app/src/main/java/com/devShahriar/trackMe/MainActivity.java
+++ app/src/main/java/com/devShahriar/trackMe/MainActivity.java
@@ -118,7 +118,7 @@ public class MainActivity extends AppCompatActivity implements LocationService.L
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == REQUEST_CODE_LOCATION_PERMISSION){
+        if(requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length>0){
             if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
                 startLocationService();
             }
